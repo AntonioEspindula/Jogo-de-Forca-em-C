21ed59e9046226c75789985116265ba682656982
Antonio Espindula <156543436+AntonioEspindula@users.noreply.github.com>
2025-05-19 17:49:02 -0300

    Add files via upload

---
+++ C - avançando na linguagem/forca.h
@@ -0,0 +1,3 @@
+#define TAMANHO_PALAVRA 20
+void abertura(), chuta(), desenhaforca(),escolhepalavrasecreta(), adicionapalavra();
+int jachutou(char letra), acertou(), enforcado(),chuteserrados(), letraexiste(char letra);
